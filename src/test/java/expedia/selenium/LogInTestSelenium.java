cef5dd1212578599c8089625e8d7d6b63be7355c
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-04-30 19:18:04 +0200

    adding wdio

--- src/test/java/expedia/selenium/LogInTestSelenium.java
+++ src/test/java/expedia/selenium/LogInTestSelenium.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utils.BaseTestSelenium;
-
 public class LogInTestSelenium extends BaseTestSelenium {
 
     private static final Logger logger = LogManager.getLogger(LogInTestSelenium.class);
