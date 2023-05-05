cf24ca858a8a6e81619f77826a385303bac2addd
Yevhenii Vakerin <utyfdfrthby08@gmail.com>
2023-05-05 18:18:16 +0200

    Fix pom

--- src/test/java/expedia/selenium/LogInTestSelenium.java
+++ src/test/java/expedia/selenium/LogInTestSelenium.java
@@ -1,18 +1,12 @@
 package expedia.selenium;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import utils.BaseTestSelenium;
+
 public class LogInTestSelenium extends BaseTestSelenium {
 
-    private static final Logger logger = LogManager.getLogger(LogInTestSelenium.class);
-
-    @Test
     public void logInTest() {
         var url = driver.getCurrentUrl();
-        logger.info(url);
         Assertions.assertEquals(url, "https://www.expedia.com/");
     }
 }
